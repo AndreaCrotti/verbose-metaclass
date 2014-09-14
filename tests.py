27c54b0993a2564edd793c8c2796a2aeff314f45
Andrea Crotti <andrea.crotti.0@gmail.com>
2014-09-14 15:29:16 +0100

    first working version of the global patching

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from verbose_metaclass.metaclass import activate, get_calls
+from verbose_metaclass.metaclass import activate, get_calls, init_calls
 
 
 def setup_module():
@@ -8,11 +8,21 @@ def setup_module():
 
 
 class TestVerboseMetaclass(unittest.TestCase):
-    def test_calling_function_adds_a_new_call(self):
+    def setUp(self):
         class Sample(object):
-            def check_call(self, arg):
+            def check_call(self, arg=None):
                 print(arg)
 
-        Sample().check_call(arg='Hello')
+        self.sample_cls = Sample
+        init_calls()
+
+    def test_calling_function_adds_a_new_call(self):
+        self.sample_cls().check_call(arg='Hello')
         calls = get_calls()
         self.assertIn('Sample.check_call', calls)
+
+    def test_sort_by_time(self):
+        obj = self.sample_cls()
+        obj.check_call(arg=1)
+        obj.check_call()
+        self.assertEqual(len(get_calls().order_by_time()), 2)
--- verbose_metaclass/metaclass.py
+++ verbose_metaclass/metaclass.py
@@ -1,8 +1,15 @@
 from datetime import datetime
+import __builtin__
+original_object = __builtin__.object
 
 
 class Calls(dict):
-    pass
+    def order_by_time(self):
+        all_events = []
+        for method, calls in self.items():
+            all_events.extend((method, c) for c in calls)
+
+        return sorted(all_events, key=lambda x: x[1])
 
 GLOBAL_CALLS = Calls()
 
@@ -10,27 +17,31 @@ GLOBAL_CALLS = Calls()
 
 class MetaVerbose(type):
     def __new__(mcs, name, bases, dict):
-        def new_getattribute(self, attr):
-            cls = object.__getattribute__(self, '__class__')
-            key = '{}.{}'.format(cls, attr)
-
-            if key not in GLOBAL_CALLS:
-                GLOBAL_CALLS[key] = []
-
-            GLOBAL_CALLS[key].append(datetime.utcnow())
-            return super(self.__class__, self).__getattribute__(attr)
-
-        dict['__getattribute__'] = new_getattribute
         return type.__new__(mcs, name, bases, dict)
 
 
-class NewBase(object):
+class NewBase(original_object):
     __metaclass__ = MetaVerbose
+
+    def __getattribute__(self, attr):
+        cls = original_object.__getattribute__(self, '__class__')
+        key = '{}.{}'.format(cls.__name__, attr)
+
+        if key not in GLOBAL_CALLS:
+            GLOBAL_CALLS[key] = []
+
+        GLOBAL_CALLS[key].append(datetime.utcnow())
+        return original_object.__getattribute__(self, attr)
 
 
 def activate():
     import __builtin__
     __builtin__.object = NewBase
+
+
+def init_calls():
+    global GLOBAL_CALLS
+    GLOBAL_CALLS = Calls()
 
 
 def get_calls():
